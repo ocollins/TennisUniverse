e3fc9b4a2612b6f356f1fc35093255c5c1eaefe5
ocollins <ocollins@madisoncollege.edu>
2017-04-15 23:25:45 -0500

    working on services per person dao

--- src/main/java/edu/matc/persistence/PersonServiceDao.java
+++ src/main/java/edu/matc/persistence/PersonServiceDao.java
@@ -10,6 +10,7 @@ import org.hibernate.Transaction;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +38,34 @@ public class PersonServiceDao {
             session.close();
         }
         return services;
+    }
+
+    /**
+     * retrieve all services for the person for current month
+     *
+     * @param id the person's id
+     * @param startDate Start date for the monthly statement
+     * @param endDate End date for the monthly statement
+     * @return person
+     */
+    public List<PersonService> getPersonService(int id, Date startDate, Date endDate) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        List <PersonService> personServiceList = null;
+
+        Criteria criteria = session.createCriteria(PersonService.class);
+        criteria.add(Restrictions.eq("personServiceId", id));
+        criteria.add(Restrictions.ge("serviceDate", startDate));
+        criteria.add(Restrictions.le("serviceDate", endDate));
+
+        try {
+            personServiceList = criteria.list();
+        } catch (HibernateException he) {
+            logger.info("Hibernate Exception " + he);
+        } finally {
+            session.close();
+        }
+
+        return personServiceList;
     }
 
 
--- src/test/java/edu/matc/persistence/PersonServiceDaoTest.java
+++ src/test/java/edu/matc/persistence/PersonServiceDaoTest.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -23,10 +24,23 @@ public class PersonServiceDaoTest {
     }
 
     @Test
-    public void getAllPersons() throws Exception {
+    public void getAllPersonsServices() throws Exception {
         List<PersonService> personServicesList = dao.getAllServices();
         logger.info("Total services " + personServicesList.size());
         assertTrue(personServicesList.size() > 0);
     }
+
+    @Test
+    public void getServicesForPerson() throws Exception {
+        List<PersonService> personServicesList =
+                dao.getPersonService(101, Date.valueOf("2017-04-01"), Date.valueOf("2017-04-31"));
+        logger.info("inside the test method " + personServicesList.size());
+
+        for(PersonService personService : personServicesList) {
+            logger.info(personService.getPersonServiceId() + " " + personService.getServiceDate());
+        }
+        //assertTrue(personServicesList.size() > 0);
+    }
+
 
 }
