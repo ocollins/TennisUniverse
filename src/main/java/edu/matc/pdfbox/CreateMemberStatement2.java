5fe9642acfa884edf2f9d25175c0a9e4b6bacecd
ocollins <ocollins@madisoncollege.edu>
2017-04-20 20:43:48 -0500

    some more pdf

--- src/main/java/edu/matc/pdfbox/CreateMemberStatement2.java
+++ src/main/java/edu/matc/pdfbox/CreateMemberStatement2.java
@@ -5,24 +5,30 @@ package edu.matc.pdfbox;
  */
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.List;
 
 import edu.matc.entity.Person;
+import edu.matc.entity.PersonService;
 import edu.matc.memberstatement.CalculateMonthlyStatement;
 import edu.matc.persistence.PersonDao;
 import edu.matc.persistence.PersonServiceDao;
 import org.apache.log4j.Logger;
+import org.apache.pdfbox.contentstream.PDFGraphicsStreamEngine;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.rendering.PageDrawer;
 
 public class CreateMemberStatement2 {
 
@@ -48,6 +54,7 @@ public class CreateMemberStatement2 {
 		insertImage(contentStream, document);
 		contentStream.setLeading(20);
 
+		//Create a page title in the header
 		createHeader(contentStream);
 
 		//Add a line after the header
@@ -65,15 +72,6 @@ public class CreateMemberStatement2 {
 		contentStream.showText("With billing questions please call Tennis Universe at (608) 574 3397");
 		contentStream.newLine();
 
-		//Reset X-starting position
-		//contentStream.newLineAtOffset(startXPosition, startYPosition);
-
-		//!!!!!!! input data
-//		int memberId = 101;
-//		Date startDate = Date.valueOf("2017-04-01");
-//		Date endDate = Date.valueOf("2017-04-30");
-		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		String printDate = dateTimeFormatter.format(LocalDate.now());
 
@@ -83,18 +81,22 @@ public class CreateMemberStatement2 {
 		contentStream.setFont(PDType1Font.HELVETICA, 12);
 		String firstName = personDao.getPerson(memberId).getFirstName();
 		String lastName = personDao.getPerson(memberId).getLastName();
-		contentStream.showText(firstName + " "+ lastName + "\052");
+		contentStream.newLineAtOffset(150, 0);
+		contentStream.showText(firstName + " "+ lastName );
 
 		contentStream.newLine();
+		contentStream.newLineAtOffset(0, 0);
 		contentStream.setFont(PDType1Font.HELVETICA_BOLD, 14);
 		contentStream.showText("Customer Account: ");
 		contentStream.setFont(PDType1Font.HELVETICA, 12);
+		contentStream.newLineAtOffset(50, 0);
 		contentStream.showText(String.valueOf(personDao.getPerson(memberId).getPersonId()));
 
 		contentStream.newLine();
 		contentStream.setFont(PDType1Font.HELVETICA_BOLD, 14);
 		contentStream.showText("Billing Period: ");
 		contentStream.setFont(PDType1Font.HELVETICA, 12);
+		contentStream.newLineAtOffset(50, 0);
 		contentStream.showText(startDate + " to " + endDate);
 
 		contentStream.newLine();
@@ -102,6 +104,7 @@ public class CreateMemberStatement2 {
 		contentStream.showText("Total Amount Due: ");
 		contentStream.setFont(PDType1Font.HELVETICA, 12);
 		calculateMonthlyStatement = new CalculateMonthlyStatement();
+		contentStream.newLineAtOffset(150, 0);
 		contentStream.showText(String.valueOf(calculateMonthlyStatement.calculateTotalDue(memberId, startDate, endDate)));
 		contentStream.newLine();
 
@@ -114,17 +117,25 @@ public class CreateMemberStatement2 {
 		//Draw page border
 		drawRect(contentStream, 20, 80, 570, 700);
 
-//		//Setting the non stroking color
-//		contentStream.setNonStrokingColor(Color.LIGHT_GRAY);
-//		//Drawing a rectangle
-//		contentStream.addRect(45, 680, 300, 40);
-//		//Drawing a rectangle
-//		contentStream.fill();
-//		contentStream.closeAndStroke();
+		//Draw grey box for the header
+		drawBox(contentStream, 45, 530, 500, 25);
+
+		//Print services and charges header
+		drawHeaderRow(contentStream, 540, "Service", "Date", "Charge");
+
+		//Create services detail lines
+		List<PersonService> Services = calculateMonthlyStatement.listServices(memberId, startDate, endDate);
+				float offset = 0f;
+		float startY = 430f;
+		for (PersonService service : Services) {
+			drawServicesRows(contentStream, startY + offset, service.getService().getServiceDesc(),
+					String.valueOf(service.getServiceDate()),
+					String.valueOf("$" + service.getService().getServiceCharge()));
+			offset = offset + 20;
+		}
 
 		//Closing the content stream
 		contentStream.close();
-
 		//Saving the document
 		document.save(new File("pdf/MemberStatement.pdf"));
 
@@ -151,8 +162,7 @@ public class CreateMemberStatement2 {
 	}
 
 	/**
-	 * Insert image.
-	 *
+	 * Insert image logo.
 	 * @param contentStream the content stream
 	 * @param document      the document
 	 * @throws IOException the io exception
@@ -173,12 +183,88 @@ public class CreateMemberStatement2 {
 		}
 	}
 
+	public void createMemInfoDetailLine(PDPageContentStream contentStream, float y, String label, String info)
+			throws IOException{
+
+	}
+
+	/**
+	 * Draw rect.
+	 * @param contentStream the content stream
+	 * @param x             the x
+	 * @param y             the y
+	 * @param w             the w
+	 * @param h             the h
+	 * @throws IOException the io exception
+	 */
 	public void drawRect (PDPageContentStream contentStream, float x, float y, float w, float h) throws IOException{
 		contentStream.setStrokingColor(Color.GRAY);
 		contentStream.addRect(x, y, w, h);
 		contentStream.closeAndStroke();
 
 	}
+
+	/**
+	 * Add box.
+	 * @param contentStream the content stream
+	 * @param x             the x
+	 * @param y             the y
+	 * @param w             the w
+	 * @param h             the h
+	 * @throws IOException the io exception
+	 */
+	public void drawBox(PDPageContentStream contentStream, float x, float y, float w, float h) throws IOException{
+		//Setting the non stroking color
+		contentStream.setNonStrokingColor(Color.LIGHT_GRAY);
+		//Drawing a rectangle
+		contentStream.addRect(x, y, w, h);
+		//Drawing a rectangle
+		contentStream.fill();
+		contentStream.closeAndStroke();
+	}
+
+	/**
+	 * Draw services header.
+	 * @param contentStream the content stream
+	 * @throws IOException the io exception
+	 */
+	public void drawHeaderRow(PDPageContentStream contentStream, float y, String service, String date, String charge)
+			throws IOException {
+		contentStream.beginText();
+		contentStream.setNonStrokingColor(Color.black);
+		contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+		contentStream.newLineAtOffset(55, y);
+		contentStream.showText(service);
+		contentStream.newLineAtOffset(280, 0);
+		contentStream.showText(date);
+		contentStream.newLineAtOffset(150, 0);
+		contentStream.showText(charge);
+		contentStream.newLine();
+		contentStream.endText();
+
+	}
+
+	/**
+	 * Draw services detail line.
+	 * @param contentStream the content stream
+	 * @throws IOException the io exception
+	 */
+	public void drawServicesRows(PDPageContentStream contentStream, float y, String service, String date, String charge)
+			throws IOException {
+		contentStream.beginText();
+		contentStream.setNonStrokingColor(Color.black);
+		contentStream.setFont(PDType1Font.HELVETICA, 12);
+		contentStream.newLineAtOffset(55, y);
+		contentStream.showText(service);
+		contentStream.newLineAtOffset(280, 0);
+		contentStream.showText(date);
+		contentStream.newLineAtOffset(150, 0);
+		contentStream.showText(charge);
+		contentStream.newLine();
+		contentStream.endText();
+
+	}
+
 
 
 }
